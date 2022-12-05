a7f59cc1e9e00223ca609fc7d1e5141e704663dc
Lauren <lburke522@gmail.com>
2022-12-05 08:36:27 -0500

    added mailing list bar - not functional...home page basics done, still using some filler info, will come back to change

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Categories from "../../components/Categories/Categories";
+import Contact from "../../components/Contact/Contact";
 import Featured from "../../components/Featured/Featured";
 import Slider from "../../components/Slider/Slider";
 
@@ -10,6 +11,7 @@ const Home = () => {
       <Featured type="featured" />
       <Categories />
       <Featured type="trending" />
+      <Contact />
     </div>
   );
 };
