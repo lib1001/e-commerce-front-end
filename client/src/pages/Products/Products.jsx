bc39929fd7b6b3f2df50c63657a794a25fe050e8
Lauren <lburke522@gmail.com>
2022-12-06 08:02:08 -0500

    sorting sidebar started

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -2,7 +2,44 @@ import React from 'react'
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <div className='products'>
+      <div className="left">
+        <div className="filtered">
+          <h2>Categories</h2>
+          <div className="input">
+            <input type="checkbox" id='1' value={1} />
+            <label htmlFor="1">Shoes</label>
+          </div>
+          <div className="input">
+            <input type="checkbox" id='1' value={1} />
+            <label htmlFor="1">Outerwear</label>
+          </div>
+          <div className="input">
+            <input type="checkbox" id='1' value={1} />
+            <label htmlFor="1">Accessories</label>
+          </div>
+        </div>
+        <div className="filtered">
+          <h2>Filter by Price</h2>
+          <div className="input">
+            <span>0</span>
+            <input type="range" min={0} max={1000} />
+            <span>1000</span>
+          </div>
+        </div>
+        <div className="filtered">
+          <h3>Sort by</h3>
+          <div className="input">
+            <input type="radio" id='asc' value='asc' name='price' />
+            <label htmlFor="asc">Price (Lowest First)</label>
+            </div>
+          <div className="input">
+            <input type="radio" id='asc' value='asc' name='price' />
+            <label htmlFor="asc">Price (Highest First)</label>
+            </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
